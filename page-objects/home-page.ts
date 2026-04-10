b3fefb4e95677db8b0221ade0cd9f47362d58024
Marcelo <marcelogscosta@gmail.com>
2026-04-10 19:41:32 -0300

    feat: creates pom for home

---
+++ page-objects/home-page.ts
@@ -0,0 +1,27 @@
+import { expect, type Locator, type Page } from '@playwright/test';
+
+export class HomePage {
+    readonly page: Page;
+    readonly burgerMenuButton: Locator;
+    readonly logoutLink: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.burgerMenuButton = this.page.locator("#react-burger-menu-btn");
+        this.logoutLink = this.page.locator("#logout_sidebar_link");
+    }
+
+    async goto() {
+        await this.page.goto(process.env.APP_URL! + "/inventory.html");
+    }
+
+    async clickOnMenu()
+    {
+        await this.burgerMenuButton.click();        
+    }
+
+    async logout() {
+        await this.logoutLink.click();
+    }
+}
+
