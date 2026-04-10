437b5c976bd38c16f18942d0c427459ee6a35275
Marcelo <marcelogscosta@gmail.com>
2026-04-10 00:07:28 -0300

    feat: create basic login feature and .env

---
+++ tests/login.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('login successfully', async ({ page }) => {
+  await page.goto(process.env.APP_URL);
+
+  await page.locator("#user-name").fill(process.env.APP_USERNAME);
+  await page.locator("#password").fill(process.env.APP_PASSWORD);
+
+  await page.locator("#login-button").click();
+
+  expect(page).toHaveURL(/inventory/);
+});
