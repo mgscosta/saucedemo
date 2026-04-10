d2ee365680d08debf4892294849b43670e633b02
Marcelo <marcelogscosta@gmail.com>
2026-04-10 00:18:37 -0300

    feat: migrate project to typescript

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('login successfully', async ({ page }) => {
-  await page.goto(process.env.APP_URL);
+  await page.goto(process.env.APP_URL!);
 
-  await page.locator("#user-name").fill(process.env.APP_USERNAME);
-  await page.locator("#password").fill(process.env.APP_PASSWORD);
+  await page.locator("#user-name").fill(process.env.APP_USERNAME!);
+  await page.locator("#password").fill(process.env.APP_PASSWORD!);
 
   await page.locator("#login-button").click();
 
