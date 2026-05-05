0d8daa6710413df524e0b1c992180b82dc01e0e9
Marcelo <marcelogscosta@gmail.com>
2026-05-04 21:27:58 -0300

    fix: fix failing test

--- tests/shop.spec.ts
+++ tests/shop.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '/repos/saucedemo/fixtures/shop.fixtures';
 
 test.describe('shop tests', () => {
     test('buy an item from the shop', async ({ login, navbar, products, cart, checkout}) => {
-        await products.addItemToCart("#add-to-cart-sauce-labs-backpack");
+        await products.addItemToCart("add-to-cart-sauce-labs-backpack");
         await navbar.goToCart();
         await cart.clickOnCheckout();
         await checkout.fillInformationForm("John", "Doe", "12345-678");
